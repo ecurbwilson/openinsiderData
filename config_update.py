f623617f5f7ff3b973c649ae6dde31ec80addcaa
ecurbwilson <ecurbwilson@gmail.com>
2026-05-08 12:16:28 -0400

    My own shit

--- config_update.py
+++ config_update.py
@@ -5,7 +5,7 @@ import yaml
 with open("config.yaml", "r") as read_file:
     contents = yaml.safe_load(read_file)
     contents['scraping']['start_year'] = 2026
-    contents['scraping']['start_month'] = 1
+    contents['scraping']['start_month'] = 4
     contents['filters']['min_transaction_value'] = 500000
     contents['filters']['transaction_types'] = 'P'
     print(contents)
